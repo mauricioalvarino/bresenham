65c89627d3ee89fa62614a11e1d42269242fba4e
MauricioAlvarino <est.mauricioe.alva2@unimilitar.edu.co>
2026-03-27 13:48:59 -0500

    #Inicio, Aplicacion de canva y funcion plot
    Archivos cargados aparte por error en computadores, tenia 4 comits pero por el error no se pueden subir

---
+++ script.js
@@ -0,0 +1,38 @@
+function plot(x, y) {
+    // Función para dibujar un punto en el canvas
+    const canvas = document.getElementById('canvas');   
+    const ctx = canvas.getContext('2d');
+    ctx.fillRect(x, y, 1, 1);
+}
+
+
+function bresenham(x0, y0, x1, y1, plot) {
+    // Cálculo de diferenciales y dirección del paso
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+    let err = dx - dy;
+
+    while (true) {
+        // Dibujar el punto actual
+        plot(x0, y0);
+
+        // Condición de finalización
+        if (x0 === x1 && y0 === y1) break;
+
+        let e2 = 2 * err;
+
+        // Ajuste en el eje X
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+
+        // Ajuste en el eje Y
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
